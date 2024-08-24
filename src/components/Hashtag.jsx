b4ad4a9bd965c78e25f8bfda447d0c1b10950a34
st9ho3 <148724871+st9ho3@users.noreply.github.com>
2024-08-24 12:56:33 +0300

    f

--- src/components/Hashtag.jsx
+++ src/components/Hashtag.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { hashtagBodyClassMap, hashtagTextClassMap } from './constants/HashtagColors';
 import { IoIosAdd } from "react-icons/io";
-<<<<<<< HEAD
 import HashtagStore from './HashtagStore';
 
 const Hashtag = ({ hashtag }) => {
@@ -9,12 +8,6 @@ const Hashtag = ({ hashtag }) => {
     const [hashtags, setHashtags] = useState(() => localStorage.getItem('Hashtags') ? JSON.parse(localStorage.getItem('Hashtags')) : []);
     const [createHashtag, setHashtag] = useState('');
     
-=======
-import { IoClose } from "react-icons/io5";
-
-const Hashtag = ({ hashtag }) => {
-    const [open, setOpen] = useState(false);
->>>>>>> 01be5ef010ff2ce22fa63a5a1176f5ae6d4458ab
     const openHashtagStore = () => {
         setOpen(true);
     }
@@ -22,13 +15,6 @@ const Hashtag = ({ hashtag }) => {
         setOpen(false);
     }
 
-<<<<<<< HEAD
-=======
-
-    const [hashtags, setHashtags] = useState(localStorage.getItem('Hashtags') ? JSON.parse(localStorage.getItem('Hashtags')) : []);
-    const [createHashtag, setHashtag] = useState('');
-
->>>>>>> 01be5ef010ff2ce22fa63a5a1176f5ae6d4458ab
     const handleHashtagSubmit = (e) => {
         setHashtag(e.target.value);
     }
@@ -54,42 +40,12 @@ const Hashtag = ({ hashtag }) => {
                 <IoIosAdd className='addHastagButton' />
             </div>
             {open && 
-<<<<<<< HEAD
                 <HashtagStore
                 closeHashtagStore={closeHashtagStore}
                 handleHashtagSubmit={handleHashtagSubmit}
                 createHashtag={createHashtag}
                 hashtags={hashtags}
                 updateHashtags={updateHashtags} />
-=======
-                <div className='hashtagStore'>
-                  <IoClose 
-                    className='hashCloseButton'
-                    onClick={closeHashtagStore} />
-                    <input
-                        onChange={handleHashtagSubmit}
-                        value={createHashtag} 
-                        type="text" 
-                        className='createHashtag'
-                        placeholder='Add a hashtag #' 
-                    />
-                    <hr className='hashtagBorderUp' />
-                    
-                    <div className="hashtagShowroom">
-                        {hashtags.map((hashtag, index) => {
-                            const firstLetter = hashtag[0].toLowerCase();
-
-                            return (
-                                <div key={index} className='hashtag'>
-                                    <div className={hashtagBodyClassMap[firstLetter] || null}></div>
-                                    <p className={hashtagTextClassMap[firstLetter] || null}> {hashtag} </p>
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <button className='submitHastag' disabled={!createHashtag} onClick={updateHashtags}>Add</button>
-                </div>
->>>>>>> 01be5ef010ff2ce22fa63a5a1176f5ae6d4458ab
             }
         </div>
     );
